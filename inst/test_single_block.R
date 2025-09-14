314893c149cff8a770b2b7d37905502b4c6da1ee
Christoph Sax <christoph.sax@gmail.com>
2025-09-14 09:34:05 +0200

    clean up

--- inst/test_single_block.R
+++
@@ -1,8 +0,0 @@
-library(blockr.core)
-library(blockr.seasonal)
-
-# Test single block serving with direct data
-serve(
-  new_seas_block(seas_call = "seas(x = x, x11 = list())"),
-  data = list(x = tsbox::ts_tbl(AirPassengers))
-)
